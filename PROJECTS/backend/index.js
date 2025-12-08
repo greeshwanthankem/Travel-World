66786d2aef21eddcca0dfcd77602bfad9bc0f00f
ANKEM GREESHWANTH <151426443+greeshwanthankem@users.noreply.github.com>
2025-12-09 00:50:04 +0530

    Update CORS options to allow multiple origins

--- PROJECTS/backend/index.js
+++ PROJECTS/backend/index.js
@@ -14,7 +14,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: "https://full-stack-website-hazel.vercel.app",
+  origin: ["https://full-stack-website-hazel.vercel.app",
+           "https://mytravelworld.vercel.app",
+            "http://localhost:3000",
+              "http://localhost:5173"],
+  credentials: true,
 };
 
 // database connection
